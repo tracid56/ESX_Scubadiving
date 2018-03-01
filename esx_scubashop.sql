3ca597dd128d2c61a619cd19a61ede782ead3dd3
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-03-01 14:11:35 +0200

    Update esx_scubashop.sql

--- esx_scubashop.sql
+++ esx_scubashop.sql
@@ -1,6 +1,6 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (id, name, label, limit, rare, can_remove) VALUES
+INSERT INTO `items` (id, name, label, `limit`, rare, can_remove) VALUES
 	(NULL, 'scuba', 'Sukellusvälineet', 1, 0 , 1),
 	(NULL, 'oxycan', 'Happisäiliö', 1, 0 , 1),
 	(NULL, 'usedscuba', 'Sukellusvälineet[tyhjä säiliö]', 1, 0 , 1),
